e53ec3c4c0d00515c669bc8ea351963962a002f3
Leonid <enykoruna1@gmail.com>
2022-09-08 15:59:12 +0300

    GameStore-Part-2

--- GameStore/Interfaces/IProduct.cs
+++ GameStore/Interfaces/IProduct.cs
@@ -5,7 +5,10 @@ namespace GameStore.Interfaces
     public interface IProduct
     {
         IEnumerable<Product> GetAllProducts();
+        Product GetProduct(int id);
         void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void UpdateAll(Product[] products);
+        void DeleteProduct(Product product);
     }
-
 }
